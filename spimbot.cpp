b148fd55a7c2c747bc85f95e3914b5d7d3618c65
Vien <vienv2@illinois.edu>
2020-11-25 13:49:15 -0600

    Changes 1 function name

--- spimbot.cpp
+++ spimbot.cpp
@@ -1,2 +1,21 @@
 #include "spimbot.h"
 
+std::pair<char, char> get_best_corn(char x, char y, char **k)
+{
+
+}
+
+char compute_time_to_point(char x0, char y0, char x1, char y1)
+{
+
+}
+
+char bonk_handler(char direction)
+{
+
+}
+
+bool should_solve_puzzle()
+{
+
+}
--- spimbot.h
+++ spimbot.h
@@ -1,7 +1,8 @@
 #pragma once
+#include <utility>
 
 // Vien's
-std::pair<char,char> get_best_corn(char x, char y, char **k);
+std::pair<char, char> get_best_corn(char x, char y, char **k);
 char compute_time_to_point(char x0, char y0, char x1, char y1);
 char bonk_handler(char direction);
 bool should_solve_puzzle();
@@ -10,5 +11,5 @@ bool should_solve_puzzle();
 void compute_direction(char x0, char y0, char x1, char y1);
 void build_silo(char x, char y);
 void travel_to_point(char x, char y);
-void send_mb_to_xy();
-std::pair<char,char> compute_silo_xy();
+void send_mb_to_point();
+std::pair<char, char> compute_silo_xy();
