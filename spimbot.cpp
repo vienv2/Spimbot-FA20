660586b80a3487118516cce284909b0e0313543c
Tino Caer <ccaer2@linux-a2.ews.illinois.edu>
2020-11-25 17:17:12 -0600

    Add empty functions

--- spimbot.cpp
+++ spimbot.cpp
@@ -19,3 +19,23 @@ bool should_solve_puzzle()
 {
 
 }
+
+void compute_direction(char x0, char y0, char x1, char y1) {
+
+}
+
+void build_silo(char x, char y) {
+
+}
+
+void travel_to_point(char x, char y) {
+
+}
+
+void send_mb_to_point(char x, char y) {
+
+}
+
+std::pair<char, char> compute_silo_xy() {
+  
+}
