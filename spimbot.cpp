51c13aa593d1934785f1abc4b044f50baf3dd07d
Vien <vienv2@illinois.edu>
2020-11-25 23:42:59 -0600

    Skeleton code

--- spimbot.cpp
+++ spimbot.cpp
@@ -1,21 +1,39 @@
 #include "spimbot.h"
 
+// Vien's
 std::pair<char, char> get_best_corn(char x, char y, char **k)
 {
-
 }
 
 char compute_time_to_point(char x0, char y0, char x1, char y1)
 {
-
 }
 
 char bonk_handler(char direction)
 {
-
 }
 
-bool should_solve_puzzle()
+bool should_solve_puzzle(char eta)
 {
+}
 
+// Tino's
+void compute_direction(char x0, char y0, char x1, char y1)
+{
+}
+
+void build_silo(char x, char y)
+{
+}
+
+void travel_to_point(char x, char y)
+{
+}
+
+void send_mb_to_point(char x, char y)
+{
+}
+
+std::pair<char, char> compute_silo_xy()
+{
 }
--- spimbot.h
+++ spimbot.h
@@ -5,7 +5,7 @@
 std::pair<char, char> get_best_corn(char x, char y, char **k);
 char compute_time_to_point(char x0, char y0, char x1, char y1);
 char bonk_handler(char direction);
-bool should_solve_puzzle();
+bool should_solve_puzzle(char eta);
 
 // Tino's
 void compute_direction(char x0, char y0, char x1, char y1);
